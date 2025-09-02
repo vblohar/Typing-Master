8c4945bbe31642e7895e427a59874e2944c92740
Vaibhav L <loharvb@gmail.com>
2025-09-02 16:25:00 +0530

    Initial project created and setup daisy UI and tailwind for UI design..

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Typing-Master');
